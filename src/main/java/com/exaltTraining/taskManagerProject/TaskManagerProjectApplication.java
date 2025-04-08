66236705d4b6961d61e6ea9ee34b3ce26aea6e84
Raghad.Mustafa <s12027617@stu.najah.edu>
2025-04-08 11:16:57 +0200

    first commit

---
+++ src/main/java/com/exaltTraining/taskManagerProject/TaskManagerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.exaltTraining.taskManagerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerProjectApplication.class, args);
+	}
+
+}
